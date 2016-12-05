74d57192e96e1ed94de9cf6898d6c9846783776d
Cristian Guraliuc <guraliucc@gmail.com>
2016-12-05 14:21:56 +0200

    [APP] Add authorization on endpoints

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   REDIS_URL: process.env.REDIS_URL,
   // 5 mins (300 seconds)
-  CACHE_EXPIRE_TIME: process.env.CACHE_EXPIRE_TIME || 5 * 60
+  CACHE_EXPIRE_TIME: process.env.CACHE_EXPIRE_TIME || 5 * 60,
+  JWT_SECRET: process.env.JWT_SECRET
 }
